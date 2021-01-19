33bf469d12c38c9f4627e3ed86c0f9d819840f68
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-01-18 16:17:50 -0800

    list rendering

--- main.js
+++ main.js
@@ -3,7 +3,16 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             description: 'A fuzzy pair of socks.',
-            image: './assets/images/vmSocks-green-onWhite.jpg'
+            image: './assets/images/vmSocks-green-onWhite.jpg',
+            inventory: 100,
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+            ],
+            sizes: ['S', 'M', 'L', 'XL',
+        'XLL']
         }
     }
 })
