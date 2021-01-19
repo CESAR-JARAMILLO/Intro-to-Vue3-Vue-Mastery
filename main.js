361aef512152f3249fadb476773709135e5bcebd
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-01-18 19:41:31 -0800

    event handling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
             description: 'A fuzzy pair of socks.',
             image: './assets/images/vmSocks-green-onWhite.jpg',
@@ -8,11 +9,22 @@ const app = Vue.createApp({
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' }
+                { id: 2234, color: 'green', image: './assets/images/vmSocks-green-onWhite.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/vmSocks-blue-onWhite.jpg' }
             ],
             sizes: ['S', 'M', 'L', 'XL',
         'XLL']
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        removeFromCart() {
+            this.cart -= 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
